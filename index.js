c61b9ec75c57c6af0e257e2dc6b0f030dc028e9a
Daniel Freitas <danfreitas97@gmail.com>
2021-11-22 20:10:44 -0300

    cors

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const routes = require('./routes')
 const passport = require('passport')
 const database = require('./models')
+const cors = require('cors')
 require('./config/passport-jwt')()
 
 
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
